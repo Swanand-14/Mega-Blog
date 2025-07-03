9da228f0c1e201b50a723b066257418c91e73e80
Swanand Takalkar <takalkarswanand@gmail.com>
2025-07-03 14:50:55 +0530

    building pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,10 @@ import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from './Login'
+import PostCard from './PostCard'
+import PostForm from './Postform/PostForm'
 export{
-    Footer,Header,Container,LogoutBtn,Logo,RTE
+    Footer,Header,Container,LogoutBtn,Logo,RTE,SignUp,Login,PostCard,PostForm
 }
